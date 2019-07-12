1d980f3d89b6f75708669ed23815ae0ffe1634ff
Harry Gordon <harry.gordon@intelligentmobile.com>
2019-07-12 14:19:15 +0100

    Added project.

---
+++ MvvmCrossGestureSample/MvvmCrossGestureSample/MvvmCrossGestureSample.Droid/FormsApplicationActivity.cs
@@ -0,0 +1,37 @@
+﻿// ---------------------------------------------------------------
+// <author>Paul Datsyuk</author>
+// <url>https://www.linkedin.com/in/pauldatsyuk/</url>
+// ---------------------------------------------------------------
+
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using MvvmCross.Forms.Platforms.Android.Views;
+
+namespace MvvmCrossGestureSample.Droid
+{
+    [Activity(Label = "FormsApplicationActivity",
+              ScreenOrientation = ScreenOrientation.Portrait,
+              LaunchMode = LaunchMode.SingleTask)]
+    public class FormsApplicationActivity : MvxFormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            ToolbarResource = Resource.Layout.Toolbar;
+            TabLayoutResource = Resource.Layout.Tabbar;
+
+            base.OnCreate(bundle);
+
+            MR.Gestures.Android.Settings.LicenseKey = "ALZ9-BPVU-XQ35-CEBG-5ZRR-URJQ-ED5U-TSY8-6THP-3GVU-JW8Z-RZGE-CQW6";
+
+            Xamarin.Essentials.Platform.Init(this, bundle);
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ MvvmCrossGestureSample/MvvmCrossGestureSample/MvvmCrossGestureSample.iOS/Setup.cs
@@ -0,0 +1,34 @@
+﻿// ---------------------------------------------------------------
+// <author>Paul Datsyuk</author>
+// <url>https://www.linkedin.com/in/pauldatsyuk/</url>
+// ---------------------------------------------------------------
+
+using MvvmCross;
+using MvvmCross.Forms.Platforms.Ios.Core;
+using MvvmCross.Logging;
+using Serilog;
+
+namespace MvvmCrossGestureSample.iOS
+{
+    public class Setup : MvxFormsIosSetup<Core.MvxApp, Core.FormsApp>
+    {
+        protected override void InitializeFirstChance()
+        {
+            base.InitializeFirstChance();
+
+            Mvx.RegisterSingleton<Core.Services.ILocalizeService>(() => new Services.LocalizeService());
+        }
+
+        public override MvxLogProviderType GetDefaultLogProviderType() => MvxLogProviderType.Serilog;
+
+        protected override IMvxLogProvider CreateLogProvider()
+        {
+            Log.Logger = new LoggerConfiguration()
+                        .MinimumLevel.Debug()
+                        .WriteTo.NSLog()
+                        .CreateLogger();
+
+            return base.CreateLogProvider();
+        }
+    }
+}
